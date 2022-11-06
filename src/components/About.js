4a0b70170126e62e10306ea09f9fedebe5d0a9d7
Jordin Bryant <jordintaylor095@gmail.com>
2022-11-06 17:04:42 -0500

    added new css

--- src/components/About.js
+++ src/components/About.js
@@ -12,7 +12,7 @@ const About = () => {
             <p>I have recently graduated from a Bootcamp through General Assembly where I learned fullstack web development. Before this, I did about 3 months of self-study, including Codecademy's HTML and CSS courses. I'm currently studying Flutter with Dart in a Development Bootcamp! As you can imagine, I am learning something new everyday and am excited to continue this journey. </p>
             <h4>Prior to entering the tech world . . .</h4>
             <p>I've worn many different hats, as a Flight Attendant, Marketing Representative and a Cosmetologist. These experiences were very fulfilling and I was able to serve others, which I love!  I believe a team-focused and transparent communication style, as well as the ability to thrive and adapt in resource-strapped environments also make me a great developer  </p>
-            <h4>When I'm not coding...</h4>
+            <h4>In my free time...</h4>
             <p>I love to travel around the US, cooking French cuisine, and most of all keeping up with my physical fitness via pilates and weight training.</p>
         </div>
         </div>
